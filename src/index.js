b3b81e6e0a4b245cec501e7949cbdd67a82408a2
evgeniyashin <106561672+evgeniyashin@users.noreply.github.com>
2022-08-13 02:22:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 module.exports = function towelSort (matrix) {
   resultArr = [];
   if (typeof matrix === 'undefined' || matrix.length === 0) return [];
-  for (i = 0; i < matrix.length; i++) {
+  for (let i = 0; i < matrix.length; i++) {
     i % 2 === 0 ?  resultArr.push(...matrix[i]) : resultArr.push(...matrix[i].reverse());
   }
   return resultArr;  
